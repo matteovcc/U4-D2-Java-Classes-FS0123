d738cc6136091332a7d3e08b0a7151770dc8b646
matteovcc <matteovcc99@gmail.com>
2023-05-03 13:27:56 +0200

    edit

--- src/Rettangoli.java
+++ src/Rettangoli.java
@@ -15,4 +15,9 @@ public class Rettangoli {
 		this.area = base * altezza;
 		System.out.println("l'area è" + " " + this.area);
 	}
+	public void Perimetro() {
+		this.perimetro= (base+altezza)*2;
+		System.out.println("il perimetro è" + " " + this.perimetro);
+	}
+	
 }
